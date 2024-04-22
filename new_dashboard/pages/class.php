637715280a399ced62f9c754cbb4b1b9938890de
Roshan Thapa <roshanrays236@gmail.com>
2024-04-22 07:25:54 +0545

    solved file % content simultaneouly upload error

--- new_dashboard/pages/class.php
+++ new_dashboard/pages/class.php
@@ -40,7 +40,19 @@ $ra = $pdo -> query($qa);
 while($rowa = $ra ->fetch()){
     $assign = $rowa['content'];
     $assigned_on = $rowa['assigned_on'];
-    $assignmentlist .= "<div class = 'assign'><div class = 'assigned_on'>$assigned_on</div>$assign</div>"; 
+    $filename = $rowa['filenam'];
+    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+    $pictures = array('jpg', 'jpeg', 'png','gif');
+    if(in_array($extension, $pictures)){
+        $assignmentlist .= "<div class = 'assign'><div class = 'assigned_on'>$assigned_on</div>$assign<div><a href='uploads/$filename' target = '_blank'><img src = 'uploads/$filename' width = '400px' height = 'auto'></a></div></div>";
+    }
+    elseif ($extension) {
+        $assignmentlist .= "<div class = 'assign'><div class = 'assigned_on'>$assigned_on</div>$assign<div><a href = 'uploads/$filename' target = '_blank' ><i class='bx bx-file' ></i>$filename</a></div></div>";
+    }
+    else {
+        $assignmentlist .= "<div class = 'assign'><div class = 'assigned_on'>$assigned_on</div>$assign</div>";
+    }
+     
 }
 
 $re = "SELECT * FROM users WHERE id = '$id'";
@@ -64,7 +76,7 @@ else {
 }
 
 // CREATE ASSIGNMENT FOR TEACHER AND VIEW ONLY FOR STUDENT if case
-
+$test = '';
 echo<<<_END
 <!DOCTYPE html>
 <html lang="en">
@@ -72,6 +84,7 @@ echo<<<_END
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <style>
 aside {
   width: 20%;
@@ -121,6 +134,7 @@ i{
 .assignmenthandler{
     margin-top: 70px;
 }
+
 </style>
 
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>   
@@ -129,7 +143,7 @@ i{
     
     <div class="navbar">
        <a href="/minorproject/new_dashboard/home.php"><i class='bx bx-arrow-back bx-md' ></i></a>
-       <span class="classname">$classname</span>
+       <span class="classname">$classname $test</span>
     </div>
     <hr>
     <div class="assignmenthandler">
@@ -148,38 +162,31 @@ i{
 </body>
 </html>
 _END;
+$test = isset($_POST['file']);
+if(isset($_POST['editor1'])  || !empty($_FILES["file"]["name"])){
+    $content = $_POST['editor1'];
+    $added_on = date('Y-m-d h:i:s');
 
-if(isset($_POST['editor1'])  || isset($_FILES["file"]["name"])){
-
-    if($_FILES["file"]["name"]!= null){
+    if(!empty($_FILES['file']['name'])){
         $targetDir = "uploads/";
         $targetFile = $targetDir . basename($_FILES["file"]["name"]);
+        move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile);
+        $filename = $_FILES["file"]["name"];
+        $folder_path = $targetDir;
+
     }
     else {
         $targetDir = null;
         $targetFile = ' ';
+        $filename = null;
+        $folder_path = null;
     }
-    
-
-    $content = $_POST['editor1'];
-    $added_on = date('Y-m-d h:i:s');
-
-    if ($content!=null || move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)){
-        if($_FILES["file"]["name"] != null){
-            $filename = $_FILES["file"]["name"];
-            $folder_path = $targetDir;
-        }
-        else{
-            $filename = null;
-            $folder_path = null;
-        }
         //file name shouldn't contain quote(') sign in it
 
-        $query = "INSERT INTO $assign_tbl(content, assigned_on, filenam, folder_path) VALUES ('$content','$added_on', '$filename','$folder_path')";
-        $rs = $pdo -> query($query);
-        if ($rs){
-            echo "<script>window.location.reload()</script>";
-        }
+    $query = "INSERT INTO $assign_tbl(content, assigned_on, filenam, folder_path) VALUES ('$content','$added_on', '$filename','$folder_path')";
+    $rs = $pdo -> query($query);
+    if ($rs){
+        echo "<script>window.location.reload()</script>";
     }
 }
 ?>
